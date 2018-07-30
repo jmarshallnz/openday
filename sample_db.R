12354e056fd3df400a8baf33f8e7b61691e6d659
Jonathan Marshall <jmarshall@never.you.mind>
2018-07-30 12:40:12 +1200

    updates sample db to use a general dbuser

--- sample_db.R
+++ sample_db.R
@@ -1,14 +1,19 @@
 library(RMySQL)
 
+user  <- 'openday'
+pass  <- 'openday'
+db    <- 'openday'
+table <- 'mandm'
+
 # fetch from the database if possible
 read_samples <- function(colours) {
   values <- NULL
 
   try({
-    conn <- dbConnect(RMySQL::MySQL(), user='mandm', password='mandm', host='localhost', dbname='mandm')
-
+    conn <- dbConnect(RMySQL::MySQL(), user=user, password=pass, host='localhost', dbname=db)
+    
     fields <- paste(c(colours, "year"), collapse=",")
-    values <- dbGetQuery(conn, paste("SELECT ",fields, " FROM samples"))
+    values <- dbGetQuery(conn, paste("SELECT ",fields, " FROM", table))
 
     dbDisconnect(conn)
   }, silent=TRUE)
@@ -21,12 +26,12 @@ create_database <- function(colours) {
   success <- FALSE
 
   try({
-    conn <- dbConnect(RMySQL::MySQL(), user='mandm', password='mandm', host='localhost', dbname='mandm')
-
+    conn <- dbConnect(RMySQL::MySQL(), user=user, password=pass, host='localhost', dbname=db)
+    
     # create score table if not already there
     fields <- paste(c("sample_id INT PRIMARY KEY AUTO_INCREMENT", sprintf("%s INT", c(colours, "year"))), collapse=",")
 
-    res <- dbSendQuery(conn, paste("CREATE TABLE IF NOT EXISTS samples (", fields, ");"))
+    res <- dbSendQuery(conn, paste("CREATE TABLE IF NOT EXISTS", table, "(", fields, ");"))
     dbClearResult(res)
 
     dbDisconnect(conn)
@@ -42,12 +47,12 @@ write_sample <- function(colours, sample, year) {
   success <- FALSE
 
   try({
-    conn <- dbConnect(RMySQL::MySQL(), user='mandm', password='mandm', host='localhost', dbname='mandm')
-
+    conn <- dbConnect(RMySQL::MySQL(), user=user, password=pass, host='localhost', dbname=db)
+    
     cols <- paste(c(colours,"year"), collapse=",")
     vals <- paste(c(sample, year), collapse=",")
-    sql  <- paste("INSERT INTO samples(",cols,") VALUES(",vals,");")
-
+    sql  <- paste("INSERT INTO", table ,"(",cols,") VALUES(",vals,");")
+    
     res <- dbSendQuery(conn, sql)
     dbClearResult(res)
 
