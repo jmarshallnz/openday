6aaefb10ce41cbb21e55631d4af7ec649571b938
Jonathan Marshall <jmarshall@never.you.mind>
2018-07-30 15:49:53 +1200

    adds some debugging

--- noodles/sample_db.R
+++ noodles/sample_db.R
@@ -55,6 +55,7 @@ write_row <- function(columns, values) {
       cols <- paste(columns[wch], collapse=",")
       vals <- paste(values[wch], collapse=",")
       sql  <- paste("INSERT INTO", table ,"(",cols,") VALUES(",vals,");")
+      cat("SQL is:", sql, "\n")
 
       res <- dbSendQuery(conn, sql)
       dbClearResult(res)
